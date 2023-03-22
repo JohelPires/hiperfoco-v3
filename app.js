c104c0298d276970e16c67791335685c6d16458f
JohelPires <johelpires@gmail.com>
2023-03-22 09:34:06 -0400

    register controller is working

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const routes = require('./routes/tasks')
+
 const connectDB = require('./db/connect')
 const PORT = process.env.PORT || 5001
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-// add welcome page
+// authentication middleware
+// const authenticateUser = require('./middleware/authentication')
 
+// error handler
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+// ====== Routes ======
+// add welcome page
+const routes = require('./routes/tasks')
+const authRouter = require('./routes/auth')
+app.use('/auth', authRouter)
 app.use('/api/v1/tasks', routes)
 
+// ====== start server ======
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
---
+++ controllers/auth.js
@@ -0,0 +1,44 @@
+// const mongoose = require('mongoose')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+// const jwt = require('jsonwebtoken')
+
+// Register:
+// - validate name, email, password - with mongoose
+// - hash password - with bcrypt
+// - Create token
+// - Send response with token
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+
+  // movendo esse codigo para um method do UserSchema:
+  // const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+  //   expiresIn: '30d',
+  // })
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+}
+
+const login = async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Por favor informe email e senha')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError('Usuário não encontrado')
+  }
+  // compare password
+  const isPasswordCorrect = await user.comparePassword(password)
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Senha inválida.')
+  }
+  const token = user.createJWT()
+  res.json({ name: user.name, msg: 'login successful', token })
+}
+
+module.exports = { register, login }
